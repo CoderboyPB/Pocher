7eac1886f49c0e2b10413b59c79d69988b801009
Mirco Stöpke <m.stoepke@gmail.com>
2020-09-17 23:04:13 +0200

    Projektdateien hinzufügen.

---
+++ Pocher/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace Pocher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new WebClient();
+            var html = client.DownloadString("https://audionow.de/podcast/die-pochers-hier");
+
+            var matchesCollection = Regex.Matches(html, "<div class=\"podcast-episode.*>");
+            foreach (Match match in matchesCollection)
+            {
+                
+                var title = Regex.Match(match.Value, @"\d+ - ([\w\süöä]+)").Value;
+                var url = Regex.Match(match.Value, "https:(.+).mp3").Value;
+
+                client.DownloadFile(new Uri(url), $"{title}.mp3");
+            }
+        }
+    }
+}
